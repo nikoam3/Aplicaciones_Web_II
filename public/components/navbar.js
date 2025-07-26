9e3ab92df173d037e4cb425ae595d62b1a6ae82f
nikoam3 <amaya.niko@gmail.com>
2025-07-26 09:23:13 -0300

    nuevo inicio de app

--- public/components/navbar.js
+++ public/components/navbar.js
@@ -1,17 +1,15 @@
-export const renderNavbar = () => {
-    return `
-    <nav class="navbar navbar-expand-lg navbar-light bg-light text-light">
+export const renderNavbar = (user) => {
+    
+    return ` <nav class="navbar navbar-expand-lg navbar-light bg-light text-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="./home.html">Mi Sitio</a>
+            <a class="navbar-brand" href="../index.html">Inicio</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="./home.html">Inicio</a>
-                    </li>
+                ${user ? `
                     <li class="nav-item">
                         <a class="nav-link" href="./miCuenta.html">Mi cuenta</a>
                     </li>
@@ -20,14 +18,22 @@ export const renderNavbar = () => {
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="./contacto.html">Contacto</a>
-                    </li>
+                    </li> 
+                    <div class="ms-auto">
+                        <button id="logoutBtn" class="btn btn-outline"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+                            <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+                        </svg></button>
+                    </div>`
+                    : 
+                    `<li class="nav-item">
+                            <a class="nav-link" href="./pages/acceder.html">Acceder</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./pages/crearUsuario.html">Crear Cuenta</a>
+                    </ul>`}
                 </ul>
-            <div class="ms-auto">
-                    <button id="logoutBtn" class="btn btn-outline"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
-                        <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
-                    </svg></button>
-            </div>
+            
             </div>
         </div>
     </nav>
--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,9 +1,10 @@
 import { renderNavbar } from "../components/navbar.js";
+import { getUserDetails } from "../utils/sessionStorage.js";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    const user = await getUserDetails();
     const navbar = document.getElementById('navbar')
-    navbar.innerHTML += renderNavbar()
-
+    navbar.innerHTML += renderNavbar(user)
     document.getElementById('logoutBtn').addEventListener('click', (e) => {
         sessionStorage.removeItem('token')
         window.location.href = '../index.html'
